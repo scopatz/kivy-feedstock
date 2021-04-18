5af205c59d367af0e926f0bfd83ff346e90587dc
Anthony Scopatz <scopatz@gmail.com>
2021-04-18 15:18:32 -0500

    fix for me

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -ex
+
 if [ "$(uname)" == "Darwin" ]; then
   export USE_X11=0
   export USE_GSTREAMER=1
@@ -6,4 +9,6 @@ else
   export USE_X11=1
   export USE_GSTREAMER=1
 fi;
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/lib/pkgconfig"
+export C_INCLUDE_PATH="${C_INCLUDE_PATH}:${PREFIX}/include/gstreamer-1.0"
 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
